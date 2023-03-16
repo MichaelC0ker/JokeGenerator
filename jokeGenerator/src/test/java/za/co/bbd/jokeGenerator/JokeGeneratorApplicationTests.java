c9a964fa25d5e9bd70b46b6b27ba36615b3336d7
masek002 <kgotlelelo.masenamela1@vcontractor.co.za>
2023-03-16 16:44:46 +0200

    Spring template

---
+++ jokeGenerator/src/test/java/za/co/bbd/jokeGenerator/JokeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.jokeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
