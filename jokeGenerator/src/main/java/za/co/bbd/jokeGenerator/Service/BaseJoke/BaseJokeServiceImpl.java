4149f16895b1830b8e7b6d77d1795a005686d480
Michael Coker <michaelco@bbd.co.za>
2023-03-23 18:19:15 +0200

    service gets base jokes from database now

--- jokeGenerator/src/main/java/za/co/bbd/jokeGenerator/Service/BaseJoke/BaseJokeServiceImpl.java
+++ jokeGenerator/src/main/java/za/co/bbd/jokeGenerator/Service/BaseJoke/BaseJokeServiceImpl.java
@@ -1,22 +1,31 @@
 package za.co.bbd.jokeGenerator.Service.BaseJoke;
+import java.util.Optional;
 import java.util.Random;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.co.bbd.jokeGenerator.Model.BaseJoke;
+import za.co.bbd.jokeGenerator.Repository.BaseJokeRepository;
 
 @Service
 public class BaseJokeServiceImpl extends IBaseJokeService {
     //private final BaseJokeRepository repository;
+    private BaseJokeRepository bjRep;
 
+    private int getNumJokes(){
+        return bjRep.findAll().size();       //Michael bruv - change this. Kind regards, Michael.
+    }
     @Override
     public BaseJoke getBaseJoke() {
         Random random = new Random();
-        int randomId = random.nextInt(10 - 1 + 1) + 1;
-        
-        BaseJoke baseJoke = new BaseJoke(1,"basejoke");
+        int randomId = random.nextInt(getNumJokes() - 1 + 1) + 1;
 
-        return baseJoke;
-        //would use repository to get basejoke by id
-        //return repository.findById(randomId);
+       // BaseJoke baseJoke = new BaseJoke(1,"basejoke");
+        Optional<BaseJoke> bj = bjRep.findById(randomId);
+
+        return bj.orElseThrow(() -> new EntityNotFoundException("Entity not found with id " + randomId + "Not found"));
     }
+
+
 } 
